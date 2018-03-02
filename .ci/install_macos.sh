e32718a8a05de32b435db67f6a632399fd3c9311
Jeongseok Lee <jslee02@gmail.com>
2018-03-02 15:34:11 -0800

    Use brew bundle command

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -3,10 +3,4 @@
 set -e
 
 brew update > /dev/null
-
-brew install boost
-brew install dartsim
-brew install libmicrohttpd
-brew install ompl --with-eigen
-brew install tinyxml2
-brew install yaml-cpp
+brew bundle
